911d9fcabd95b6c2e3f54918de96a386fb5ecfaf
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-02-01 11:41:29 +0530

    route parameter done

--- src/index.mjs
+++ src/index.mjs
@@ -3,58 +3,77 @@ import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const mockUsers = [
+    {
+        id: 1,
+        name: 'Pritam',
+        Status: 'Active'
+    },
+    {
+        id: 2,
+        name: 'Jack',
+        Status: 'Active'
+    },
+    {
+        id: 3,
+        name: 'Donald',
+        Status: 'Active'
+    },
+    {
+        id: 4,
+        name: 'Eren',
+        Status: 'Inctive'
+    }
+]
 
 app.get('/', (request, response) => {
     response.statusCode(200).send('hello world')
 })
 app.get('/api/users', (request, response) => {
-    response.send([
-        {
-            id: '1',
-            name: 'Pritam',
-            Status: 'Active'
-        },
-        {
-            id: '2',
-            name: 'Jack',
-            Status: 'Active'
-        },
-        {
-            id: '3',
-            name: 'Donald',
-            Status: 'Active'
-        },
-        {
-            id: '4',
-            name: 'Eren',
-            Status: 'Inctive'
-        }
-    ])
+    response.send(mockUsers)
 })
-app.get('/api/products', (request, response) => {
-    response.send([
-        {
-            id: '1',
-            ProductName: 'Pritam',
-            Status: 'Processing'
-        },
-        {
-            id: '2',
-            ProductName: 'Jack',
-            Status: 'Delivered'
-        },
-        {
-            id: '3',
-            ProductName: 'Donald',
-            Status: 'Processing'
-        },
-        {
-            id: '4',
-            ProductName: 'Eren',
-            Status: 'On the Way'
-        }
-    ])
+
+app.get('/api/users/:id', (request, response) => {
+    const parseId = parseInt(request.params.id)
+    console.log(request.params)
+
+    const users = mockUsers.find(user => user.id === parseId)
+
+    if (isNaN(parseId)) {
+        response.status(400).send('Bad Request. Invalid Id')
+    }
+
+    if (!users) {
+        response.status(404).send("Users Don't Exist")
+    } else {
+        response.send(users)
+    }
 })
+
+// app.get('/api/products', (request, response) => {
+//     response.send([
+//         {
+//             id: '1',
+//             ProductName: 'Pritam',
+//             Status: 'Processing'
+//         },
+//         {
+//             id: '2',
+//             ProductName: 'Jack',
+//             Status: 'Delivered'
+//         },
+//         {
+//             id: '3',
+//             ProductName: 'Donald',
+//             Status: 'Processing'
+//         },
+//         {
+//             id: '4',
+//             ProductName: 'Eren',
+//             Status: 'On the Way'
+//         }
+//     ])
+// })
 
 
 app.listen(PORT, () => {
